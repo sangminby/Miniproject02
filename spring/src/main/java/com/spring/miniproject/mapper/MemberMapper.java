cd1582e7637a8a2a8c02d786e901e7e721a105b8
himedia.byul <himedia.byul@gmail.com>
2022-11-30 17:46:06 +0900

    Member Mapper

---
+++ spring/src/main/java/com/spring/miniproject/mapper/MemberMapper.java
@@ -0,0 +1,15 @@
+package com.spring.miniproject.mapper;
+
+import com.spring.miniproject.domain.ShopMember;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface MemberMapper {
+
+    int regMember(ShopMember shopMember);
+
+    @Select("select * from shopmember where uid=#{param1} and pw=#{param2}")
+    ShopMember selectId(String uid, String pw);
+
+}
